27c3b3ac1d51bd6bea0330a21717bdba5cb80fbe
Pratiksh Saini <pratikshsaini10@gmail.com>
2025-07-14 14:00:39 +0530

    Problem 4 & 5 Solved

--- script.js
+++ script.js
@@ -16,12 +16,27 @@ console.log(outputName);
 
 // ............................................................
 
-let inputCase = ["apple", "banana"]
+let inputCase = ["hello", "world"];
 
-let outputCase = inputCase.map((x)=>{
-    return x.toUpperCase()
-})
+let outputCase = inputCase.map((x) => {
+  return x.toUpperCase();
+});
 console.log(outputCase);
 
 // ............................................................
 
+let input = ["apple", "banana"];
+
+let output = input.map((x, index) => {
+  return x + index;
+});
+console.log(output);
+
+// ............................................................
+
+let inputTemprature = [0, 20, 100];
+
+let outputTemprature = inputTemprature.map((x) => {
+  return (x * 9 / 5) + 32;
+});
+console.log(outputTemprature);
